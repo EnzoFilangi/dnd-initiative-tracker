95b290eb1b51a8c9353af944430af307fe872ab8
EnzoFilangi <enzo.filangi@efrei.net>
2022-08-07 16:49:47 +0200

    Interactions with the monsters' HPs

--- src/app/components/monster-box/monster-box.component.ts
+++ src/app/components/monster-box/monster-box.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {Monster} from "../../interfaces/monster";
 
-import {faMinus, faPlus, faIdBadge} from "@fortawesome/free-solid-svg-icons";
+import {faHandBackFist, faBriefcaseMedical, faIdBadge, faSkull, faStaffSnake} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-monster-box',
@@ -11,9 +11,13 @@ import {faMinus, faPlus, faIdBadge} from "@fortawesome/free-solid-svg-icons";
 export class MonsterBoxComponent {
   @Input() monster: Monster | undefined;
 
-  minus = faMinus;
-  plus = faPlus;
+  hurt = faHandBackFist;
+  heal = faBriefcaseMedical;
   badge = faIdBadge;
+  skull = faSkull;
+  resurrect = faStaffSnake;
+
+  hpManipulationField: number = 0;
 
   /**
    * Opens the refsheet of the monster in a new tab
@@ -30,5 +34,38 @@ export class MonsterBoxComponent {
       return Math.max((this.monster.hp / this.monster.maxHp) * 100, 0) + "%";
     }
     return "0%";
+  }
+
+  /**
+   * Adds the value of `this.hpManipulationField` to the current health total of the monster.
+   *
+   * Capped at the max monster's HP
+   */
+  addHP(): void {
+    if (this.monster) {
+      this.monster.hp = Math.min(this.monster.maxHp, this.monster.hp + this.hpManipulationField);
+    }
+    this.hpManipulationField = 0;
+  }
+
+  /**
+   * Subtracts the value of `this.hpManipulationField` from the current health total of the monster.
+   *
+   * Capped at 0
+   */
+  removeHP(): void{
+    if (this.monster) {
+      this.monster.hp = Math.max(0, this.monster.hp - this.hpManipulationField);
+    }
+    this.hpManipulationField = 0;
+  }
+
+  /**
+   * Sets the current health of the monster to its maximum HP
+   */
+  setFullHp(): void {
+    if (this.monster) {
+      this.monster.hp = this.monster.maxHp
+    }
   }
 }
