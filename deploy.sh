d838b916eb08049ad55b3ba2db8db96fdedb450e
EnzoFilangi <enzo.filangi@efrei.net>
2022-08-26 17:50:45 +0200

    Changed docker port and added a deployment script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+exec >> /home/enzo/logs/webhooks/dnd/output.log 2>>/home/enzo/logs/webhooks/dnd/output.log
+
+printf "\n\n"
+echo "###### Deploying changes ######"
+date +'%Y-%m-%d %H:%M:%S'
+
+cd /home/enzo/dnd-initiative-tracket
+
+echo "------ Git pull ------"
+git reset --hard HEAD # Delete all changes mades locally
+git fetch --all # Fetch the changes
+git pull # Pull the changes
+
+echo "------ Rebuilding and restarting the container ------"
+docker compose up --force-recreate --build -d
+
+echo "------ Deleting unused images ------"
+docker image prune -f
+
+echo "###### Deployed! ######"
